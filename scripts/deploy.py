4af11dd53c84a79dd3f3dcca57f8aca1a37ed3fa
Noah <noahplaczek@gmail.com>
2022-01-16 19:07:11 -0500

    Abstracted pulling price feeds, created a mock price feed

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,10 +1,30 @@
-from brownie import FundMe
+from brownie import FundMe, MockV3Aggregator, network, config
 from scripts.helpful_scripts import get_account
+
 
 def deploy_fund_me():
     account = get_account()
-    fund_me = FundMe.deploy({"from": account})
-    print(fund_me.address)
+    print(account)
+    if network.show_active() != "development":
+        price_feed_address = config["networks"][network.show_active()][
+            "eth_usd_price_feed"
+        ]
+    else:
+        print(f"The active network is {network.show_active()}")
+        print("Deploying Mocks...")
+        mock_aggregator = MockV3Aggregator.deploy(
+            18, 2000000000000000000000, {"from": account}
+        )
+        price_feed_address = mock_aggregator.address
+        print("Mocks deployed!")
+    # public_source=True: allows us to verify our code in etherscan
+    # need the api setup in .env to do this
+    fund_me = FundMe.deploy(
+        price_feed_address,
+        {"from": account},
+        publish_source=config["networks"][network.show_active()].get("verify"),
+    )
+
 
 def main():
     deploy_fund_me()
--- scripts/helpful_scripts.py
+++ scripts/helpful_scripts.py
@@ -2,12 +2,18 @@ from brownie import accounts, network, config
 
 # based on the type of environment, will pull in the appropriate accounts
 def get_account():
-    if network.show_active() == "development": # brownie run scripts/deploy.py
-        return
-    else: # brownie run scripts/deploy.py --network rinkeby
+    if network.show_active() == "development":  # brownie run scripts/deploy.py
+        return accounts[0]
+    else:  # brownie run scripts/deploy.py --network rinkeby
         return accounts.add(config["wallets"]["from_key"])
+
 
 # DONT FORGET TO ADD IN YAML AND CREATE .env
 # dotenv: .env
-# wallets: 
+# wallets:
 #  from_key: ${PRIVATE_KEY}
+
+# .env
+# export PRIVATE_KEY=
+# export WEB3_INFURA_PROJECT_ID=
+# export ETHERSCAN_TOKEN=
